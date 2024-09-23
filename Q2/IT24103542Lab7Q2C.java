77cf5d5e644705a0b71248da3ebbf2ec979d044c
IT24103542 <it24103542@my.sliit.lk>
2024-09-23 19:23:13 +0530

    Q2 - C

---
+++ Q2/IT24103542Lab7Q2C.java
@@ -0,0 +1,14 @@
+public class IT24103542Lab7Q2C{
+	public static void main(String[] args){
+	int r,c,k=5;
+	for(r=1; r<=5; r++){
+		
+		for(c=1; c<=k; c++){
+			System.out.print(k);
+		}
+		
+		System.out.print("\n");
+		k = k - 1;
+	}
+	}
+}
